71fc846b516ef1ef20439f0d6f1edd37312f2937
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-01-04 18:46:07 -0500

    Improve dynamic_tabs

---
+++ demos/dynamic_tabs/README.md
@@ -0,0 +1,4 @@
+# Dynamic tabs
+- Can dynamically register tabs and contents, no matter how many item to be displayed.
+- Can be used for displaying data from public API.
+
---
+++ scripts/main.js
@@ -0,0 +1,120 @@
+/**
+ * Get icons blinking.
+ */
+$(document).ready( function() {
+
+  // Blinking eyes
+  setInterval(function() {
+      $('.flashing').fadeOut(1000).fadeIn(1000);
+  }, 2000);
+
+});
+
+
+// ----------------------------------------------------------------------- //
+// ----------------------------------------------------------------------- //
+
+
+/**
+ * The main Augular module for this website.
+ */
+(function() {
+
+  // Module declaration.
+  angular
+    .module( "app", [ ] );
+
+
+  // --------------------------------------------------------------------- //
+  // --------------------------------------------------------------------- //
+
+
+  angular
+    .module( "app" )
+    .controller( "AppController", AppController );
+
+  AppController.$inject = [ ];
+  function AppController() {
+
+    vm = this;
+
+    // Initial state.
+    vm.demos = [];
+
+    // Load demo data.
+    vm.demos = addUrlsToDemos( demos );
+
+
+    /**
+     * Generate demoUrl and githubUrl for each demo based on my convention.
+     * @param demos A JSON array of demo info.
+     */
+    function addUrlsToDemos( demos ) {
+
+      var extendedWithUrls = _.map(demos, function(demo) {
+
+        var githubBaseUrl = "https://github.com/mnishiguchi/mnishiguchi.github.io/tree/master/demos/";
+
+        // Generate urls based on my convention and create an object with them.
+        var urls = {
+          demoUrl:   [ "demos/", demo.name, "/index.html" ].join(""),
+          githubUrl: [ githubBaseUrl, demo.name, "/index.html" ].join(""),
+        }
+
+        // Extend each demo object with urls.
+        return _.extend({}, demo, urls);
+
+      });
+
+      return extendedWithUrls;
+
+    } // addUrlsToDemos
+
+  } // end AppController
+
+
+  // ---
+  // DEMO DATA
+  // ---
+
+
+  var demos = [
+      {
+        name: "binding_events",
+        type: "Angular",
+        desc: "",
+      },
+      {
+        name: "bookstore",
+        type: "Angular",
+        desc: "",
+      },
+      {
+        name: "filterable_table",
+        type: "Angular",
+        desc: "",
+      },
+      {
+        name: "gemstore",
+        type: "Angular",
+        desc: "",
+      },
+      {
+        name: "multi_transclude",
+        type: "Angular",
+        desc: "",
+      },
+      {
+        name: "dynamic_tabs",
+        type: "Angular",
+        desc: "",
+      },
+      {
+        name: "toggling_buttons",
+        type: "Angular",
+        desc: "",
+      },
+    ];
+
+
+})(); // end module
